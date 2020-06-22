468849a53b6e92e326c67f4fe79b16cc7cfb44db
Srinath <srinathsuddala@gmail.com>
2020-06-22 18:34:57 +0530

    Created Notes Details view

---
+++ note_Srinath_C0741052/note_Srinath_C0741052/NoteDetailsViewController.swift
@@ -0,0 +1,13 @@
+
+import Foundation
+import UIKit
+
+class NoteDetailsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Details"
+        // Do any additional setup after loading the view.
+    }
+    
+}
--- note_Srinath_C0741052/note_Srinath_C0741052/NotesListViewController.swift
+++ note_Srinath_C0741052/note_Srinath_C0741052/NotesListViewController.swift
@@ -5,11 +5,13 @@ class NotesListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Notes"
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(addTapped))
         // Do any additional setup after loading the view.
     }
     
     @objc func addTapped() {
+        
     }
 }
 
