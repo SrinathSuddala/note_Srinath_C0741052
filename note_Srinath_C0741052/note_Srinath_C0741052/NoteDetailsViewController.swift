3a5b900541fcd00b43dfef78a3c051e305197a2e
Srinath <srinathsuddala@gmail.com>
2020-06-22 19:08:02 +0530

    Designed details view

--- note_Srinath_C0741052/note_Srinath_C0741052/NoteDetailsViewController.swift
+++ note_Srinath_C0741052/note_Srinath_C0741052/NoteDetailsViewController.swift
@@ -4,10 +4,16 @@ import UIKit
 
 class NoteDetailsViewController: UIViewController {
     
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var descriptionTextView:UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Details"
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func addButtonTapped(_ sender: UIButton) {
     }
     
 }
