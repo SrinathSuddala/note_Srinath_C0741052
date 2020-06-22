bbff6c823c80548ebe88ed4ca35d4fc22e6ed40d
Srinath <srinathsuddala@gmail.com>
2020-06-22 18:29:10 +0530

    Added Add navigation button

---
+++ note_Srinath_C0741052/note_Srinath_C0741052/NotesListViewController.swift
@@ -0,0 +1,15 @@
+
+import UIKit
+
+class NotesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(addTapped))
+        // Do any additional setup after loading the view.
+    }
+    
+    @objc func addTapped() {
+    }
+}
+
