032698a414feb6b84b01c66179718328bfa4b0e9
Srinath <srinathsuddala@gmail.com>
2020-06-22 20:01:10 +0530

    Saved note into database

--- note_Srinath_C0741052/note_Srinath_C0741052/NoteDetailsViewController.swift
+++ note_Srinath_C0741052/note_Srinath_C0741052/NoteDetailsViewController.swift
@@ -6,6 +6,7 @@ class NoteDetailsViewController: UIViewController {
     
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var descriptionTextView:UITextView!
+    let appdelegate = UIApplication.shared.delegate as! AppDelegate
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -14,6 +15,17 @@ class NoteDetailsViewController: UIViewController {
     }
     
     @IBAction func addButtonTapped(_ sender: UIButton) {
+        saveDataInDataBase()
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    func saveDataInDataBase() {
+        guard let title = titleTextField.text, !title.isEmpty, let descriptionText = descriptionTextView.text else { return }
+        let note = Note(context: appdelegate.persistentContainer.viewContext)
+        note.title = title
+        note.desc = descriptionText
+        appdelegate.persistentContainer.viewContext.insert(note)
+        try? appdelegate.persistentContainer.viewContext.save()
     }
     
 }
