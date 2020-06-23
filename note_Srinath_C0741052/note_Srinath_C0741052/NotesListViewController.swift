a394217dba120ec6d7e0a7befd422bca6f0a954c
Srinath <srinathsuddala@gmail.com>
2020-06-23 10:58:52 +0530

    Added notes list table view

--- note_Srinath_C0741052/note_Srinath_C0741052/NotesListViewController.swift
+++ note_Srinath_C0741052/note_Srinath_C0741052/NotesListViewController.swift
@@ -2,10 +2,15 @@
 import UIKit
 
 class NotesListViewController: UIViewController {
+    
+    @IBOutlet weak var noteTableView: UITableView!
+    var notes: [Note] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Notes"
+        noteTableView.register(UITableViewCell.self,
+                           forCellReuseIdentifier: "Cell")
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(addTapped))
         // Do any additional setup after loading the view.
     }
@@ -17,3 +22,24 @@ class NotesListViewController: UIViewController {
     }
 }
 
+
+// MARK: - UITableViewDataSource
+extension NotesListViewController: UITableViewDataSource {
+  
+  func tableView(_ tableView: UITableView,
+                 numberOfRowsInSection section: Int) -> Int {
+    
+    return notes.count
+  }
+  
+  func tableView(_ tableView: UITableView,
+                 cellForRowAt indexPath: IndexPath)
+    -> UITableViewCell {
+        
+        let cell =
+            tableView.dequeueReusableCell(withIdentifier: "Cell",
+                                          for: indexPath)
+        cell.textLabel?.text = notes[indexPath.row].title
+        return cell
+    }
+}
