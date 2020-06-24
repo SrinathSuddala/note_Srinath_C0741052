50cd8d05e816f9bf2ed6993b16cd2fa4ad99f8b6
Srinath <srinathsuddala@gmail.com>
2020-06-25 00:03:45 +0530

    Added annotations on map

--- note_Srinath_C0741052/note_Srinath_C0741052/MapViewController.swift
+++ note_Srinath_C0741052/note_Srinath_C0741052/MapViewController.swift
@@ -7,9 +7,9 @@ class MapViewController: UIViewController {
     var notes: [Note]?
     @IBOutlet weak var mapView: MKMapView!
     fileprivate let locationManager: CLLocationManager = {
-       let manager = CLLocationManager()
-       manager.requestWhenInUseAuthorization()
-       return manager
+        let manager = CLLocationManager()
+        manager.requestWhenInUseAuthorization()
+        return manager
     }()
     
     
@@ -18,35 +18,48 @@ class MapViewController: UIViewController {
         title = "Map"
         mapView.showsUserLocation = true
         currentLocationSetUp()
+        addNotesAnnotations()
         // Do any additional setup after loading the view.
     }
     
-    //MARK: - Helper Method
+    func addNotesAnnotations() {
+        guard let notes = notes else {return}
+        for note in notes {
+            addAnnotationOnLocation(pointedCoordinate: CLLocationCoordinate2D(latitude: note.lat, longitude: note.long), title: note.title)
+        }
+    }
+    
+    func addAnnotationOnLocation(pointedCoordinate: CLLocationCoordinate2D, title: String?) {
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = pointedCoordinate
+        mapView.addAnnotation(annotation)
+    }
+    
     func currentLocationSetUp() {
-       locationManager.delegate = self
-       locationManager.desiredAccuracy = kCLLocationAccuracyBest
-       if #available(iOS 11.0, *) {
-          locationManager.showsBackgroundLocationIndicator = true
-       }
-       locationManager.startUpdatingLocation()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        if #available(iOS 11.0, *) {
+            locationManager.showsBackgroundLocationIndicator = true
+        }
+        locationManager.startUpdatingLocation()
     }
     
     func showNotesOnMap() {
         
     }
-
+    
 }
 
 extension MapViewController: CLLocationManagerDelegate {
-     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let location = locations.last! as CLLocation
         let currentLocation = location.coordinate
         let coordinateRegion = MKCoordinateRegion(center: currentLocation, latitudinalMeters: 1500, longitudinalMeters: 1500)
         mapView.setRegion(coordinateRegion, animated: true)
         locationManager.stopUpdatingLocation()
-     }
-     
-     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print(error.localizedDescription)
-     }
+    }
 }
