ad1a1f5925cae2e25eb303a4a2be08efb507604a
Srinath <srinathsuddala@gmail.com>
2020-06-24 19:38:02 +0530

    Added image picker to details view controller

--- note_Srinath_C0741052/note_Srinath_C0741052/NoteDetailsViewController.swift
+++ note_Srinath_C0741052/note_Srinath_C0741052/NoteDetailsViewController.swift
@@ -8,6 +8,8 @@ class NoteDetailsViewController: UIViewController {
     @IBOutlet weak var descriptionTextView:UITextView!
     @IBOutlet weak var imageButton: UIButton!
     @IBOutlet weak var recordButton: UIButton!
+    var selectedImage: UIImage?
+    var imagePicker: ImagePicker!
     
     let appdelegate = UIApplication.shared.delegate as! AppDelegate
     
@@ -16,6 +18,7 @@ class NoteDetailsViewController: UIViewController {
         title = "Details"
         let dismissTap = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboardTapOfMainView))
         self.view.addGestureRecognizer(dismissTap)
+        self.imagePicker = ImagePicker(presentationController: self, delegate: self)
         // Do any additional setup after loading the view.
     }
     
@@ -29,7 +32,7 @@ class NoteDetailsViewController: UIViewController {
     }
     
     @IBAction func tappedImageButton(_ sender: UIButton) {
-        
+        self.imagePicker.present(from: sender)
     }
     
     func saveDataInDataBase() {
@@ -37,8 +40,37 @@ class NoteDetailsViewController: UIViewController {
         let note = Note(context: appdelegate.persistentContainer.viewContext)
         note.title = title
         note.desc = descriptionText
+        note.date = Date()
+        if let image = selectedImage,let imageData = image.jpeg(.low) {
+            note.image = imageData.base64EncodedString(options: .lineLength64Characters)
+        }
         appdelegate.persistentContainer.viewContext.insert(note)
         try? appdelegate.persistentContainer.viewContext.save()
     }
     
+}
+
+extension NoteDetailsViewController: ImagePickerDelegate {
+
+    func didSelect(image: UIImage?) {
+        selectedImage = image
+    }
+}
+
+
+extension UIImage {
+    enum JPEGQuality: CGFloat {
+        case lowest  = 0
+        case low     = 0.25
+        case medium  = 0.5
+        case high    = 0.75
+        case highest = 1
+    }
+
+    /// Returns the data for the specified image in JPEG format.
+    /// If the image object’s underlying image data has been purged, calling this function forces that data to be reloaded into memory.
+    /// - returns: A data object containing the JPEG data, or nil if there was a problem generating the data. This function may return nil if the image has no data or if the underlying CGImageRef contains data in an unsupported bitmap format.
+    func jpeg(_ jpegQuality: JPEGQuality) -> Data? {
+        return jpegData(compressionQuality: jpegQuality.rawValue)
+    }
 }
