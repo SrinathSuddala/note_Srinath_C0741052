85d840d8ae005bce555a0cf7ff517a7ff980d26c
Srinath <srinathsuddala@gmail.com>
2020-06-25 21:53:17 +0530

    Added save button

--- note_Srinath_C0741052/note_Srinath_C0741052/CategoryDetailViewController.swift
+++ note_Srinath_C0741052/note_Srinath_C0741052/CategoryDetailViewController.swift
@@ -4,11 +4,16 @@ import UIKit
 class CategoryDetailViewController: UIViewController {
 
     @IBOutlet weak var titleTextField : UITextField!
+    let appdelegate = UIApplication.shared.delegate as! AppDelegate
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(saveTapped))
         // Do any additional setup after loading the view.
+    }
+    
+    @objc func saveTapped() {
+        
     }
     
 
