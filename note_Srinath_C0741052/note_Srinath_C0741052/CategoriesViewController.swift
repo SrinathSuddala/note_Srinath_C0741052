8f4eeeefa67af12949300621d4a2d89d8712339e
Srinath <srinathsuddala@gmail.com>
2020-06-25 21:44:21 +0530

    Adde categories list view

---
+++ note_Srinath_C0741052/note_Srinath_C0741052/CategoriesViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class CategoriesViewController: UIViewController {
+    
+    @IBOutlet weak var categoryTable: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
