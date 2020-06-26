8c892814e60e73d63c2cddc58e1fd7c70812e755
Srinath <srinathsuddala@gmail.com>
2020-06-26 08:05:39 +0530

    Updated predicate for category list

--- note_Srinath_C0741052/Note+CoreDataClass.swift
+++ note_Srinath_C0741052/Note+CoreDataClass.swift
@@ -5,7 +5,7 @@ import CoreData
 public class Note: NSManagedObject {
     @nonobjc public class func fetchRequest(with searchText: String, categoryUuid: UUID) -> NSFetchRequest<Note> {
         let fetch = NSFetchRequest<Note>(entityName: "Note")
-        fetch.predicate = NSPredicate(format: "title contains[c] %@ OR desc contains[c] %@ AND category.uuid == %@", argumentArray: [searchText, searchText, categoryUuid])
+        fetch.predicate = NSPredicate(format: "(title contains[c] %@ OR desc contains[c] %@) AND category.uuid == %@", argumentArray: [searchText, searchText, categoryUuid])
         fetch.sortDescriptors = [NSSortDescriptor(key: "date", ascending: false)]
         
         return fetch
